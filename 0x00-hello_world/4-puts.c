4183a36db2b57d288c2e7ec818396719cf063554
mosessifuna20 <mosessifuna2003@gmail.com>
2023-08-10 13:45:35 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include<studio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
-{
 	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}
+	return (0)
